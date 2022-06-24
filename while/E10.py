3f60c4c9341994c5c0c66f27f71b54748f2a9172
lamarcelo <marcelo.lamonica17@gmail.com>
2022-06-24 10:26:24 -0300

    Correção exercício

--- while/E10.py
+++ while/E10.py
@@ -4,16 +4,28 @@ pedra = 0
 papel = 1
 tesoura = 2
 jogar_novamente = "Sim"
+vitoriasP = 0
+vitoriasR = 0
+
 while (jogar_novamente == "Sim"):
-    jogador1 = int( input("0 - Pedra\n1 - Papel\n2 - Tesoura\nEscolha sua opção: " ))
-    jogador2 = random.randrange(0, 2)
-    if (0 <= jogador1 <= 2) and (0 <= jogador2 <= 2):
-        if (jogador1 == jogador2):
-            print("Vish... Deu empate!" )
-        elif (jogador1 - jogador2 == -2) or (jogador1 - jogador2 == 1):
-            print("Você ganhou do robo!" )
+    if vitoriasP < 3:
+        if vitoriasR < 3:
+            jogador1 = int( input("0 - Pedra\n1 - Papel\n2 - Tesoura\nEscolha sua opção: " ))
+            jogador2 = random.randrange(0, 2)
+            if (0 <= jogador1 <= 2) and (0 <= jogador2 <= 2):
+                if (jogador1 == jogador2):
+                    print("Vish... Deu empate!\n" )
+                elif (jogador1 - jogador2 == -2) or (jogador1 - jogador2 == 1):
+                    vitoriasP = vitoriasP + 1
+                    print("Você ganhou e está com {} ponto(s)!\n".format(vitoriasP))
+                else:
+                    vitoriasR = vitoriasR + 1
+                    print("O robo ganhou e está com {} ponto(s)!\n".format(vitoriasR))
+            else:
+                print("Opção inválida." )
         else:
-            print("O robo ganhou :( Tente novamente!" )
+            print('Você perder :( O robo ganhou 3 pontos e venceu de você!')
+            break
     else:
-        print("Opção inválida." )
-    jogar_novamente = input("Você quer tentar novamente? " )
+        print('Você ganhou! Você ganhou 3 pontos e venceu do robo!')
+        break
